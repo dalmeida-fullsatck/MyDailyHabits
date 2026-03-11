20cd58d71a8bb65beb0346dd472255cef713f462
dalmeida-fullsatck <prfsl.davi@gmail.com>
2026-03-11 14:54:02 -0400

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import  Footer from './components/Footer.jsx'
+function App() {
+
+
+  return (
+    <>
+      <div>  
+        <h1> Meus hábitos diários</h1>
+        <p>Gerencie meus hábitos diários de forma simples e visual</p>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return(
+        <footer>
+            <small>Desenvolvimento Front-End com react</small>
+        </footer>
+    )
+}
+export default Footer
